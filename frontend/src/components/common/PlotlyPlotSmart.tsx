51b2a5477672aa57addf7c208f579e0ddba1dabe
Lars Eggimann <lars.eggimann@gmail.com>
2025-04-12 23:50:28 +0200

    cool plot!

--- frontend/src/components/common/PlotlyPlotSmart.tsx
+++ frontend/src/components/common/PlotlyPlotSmart.tsx
@@ -1,62 +1,146 @@
-import React, { useState, useEffect } from 'react';
-import Plot from 'react-plotly.js';
-import * as Plotly from "plotly.js";
+import React, { useState, useEffect } from 'react'
+import Plot from 'react-plotly.js'
+import * as Plotly from 'plotly.js'
+import { useColorModeValue } from '../../components/ui/color-mode'
 
 interface TimeSeriesChartProps {
-  xData: () => number[] | string[];
-  yData: () => number[];
-  animationDuration?: number; // Duration in milliseconds
+  xData: () => number[] | string[]
+  yData: () => number[]
+  title?: string
+  xAxisLabel?: string
+  yAxisLabel?: string
+  animationDuration?: number // duration in milliseconds
+  lineColor?: string
+  hoverTemplate?: string
+  height?: string | number
 }
 type PlotlyFigure = {
-  data: Plotly.Data[];
-  layout: Partial<Plotly.Layout>;
-  frames?: Plotly.Frame[];
-  config?: Partial<Plotly.Config>;
-};
+  data: Plotly.Data[]
+  layout: Partial<Plotly.Layout>
+  frames?: Plotly.Frame[] | null
+  config?: Partial<Plotly.Config>
+}
 
-
-const TimeSeriesChart: React.FC<TimeSeriesChartProps> = ({ 
-  xData, 
-  yData, 
-  animationDuration = 500 
+const TimeSeriesChart: React.FC<TimeSeriesChartProps> = ({
+  xData,
+  yData,
+  title = 'Time Series Chart',
+  xAxisLabel = 'Time',
+  yAxisLabel = 'Value',
+  animationDuration = 300,
+  hoverTemplate = '<b>X:</b> %{x}<br><b>Y:</b> %{y}<extra></extra>',
+  lineColor,
+  height = '600px',
 }) => {
+  const textColor = useColorModeValue('#1A202C', '#E2E8F0')
+  const gridColor = useColorModeValue('#CBD5E0', '#4A5568')
+  const defaultLineColor = useColorModeValue(
+    'rgba(234, 104, 104, 0.9)',
+    'rgba(234, 104, 104, 0.9)',
+  )
+  const tooltipBgColor = useColorModeValue(
+    'rgba(255, 255, 255, 0.9)',
+    'rgba(26, 32, 44, 0.9)',
+  )
+  const tooltipBorderColor = useColorModeValue('#CBD5E0', '#4A5568')
+  const bgColor = 'transparent'
 
-  const getPlotData = (xVals: number[] | string[], yVals: number[]): Plotly.Data[] => {
-    return [{
-      x: xVals,
-      y: yVals,
-      type: 'scatter',
-      mode: 'lines',
-      line:{
-        shape: 'spline',
-        smoothing: 0.8,
-      }
-    }]
+  const finalLineColor = lineColor || defaultLineColor
+
+  const customData = xData().map((x, y) => [
+    new Date(x).toLocaleTimeString(),
+    yData()[y],
+  ])
+
+  const getPlotData = (
+    xVals: number[] | string[],
+    yVals: number[],
+  ): Plotly.Data[] => {
+    return [
+      {
+        x: xVals,
+        y: yVals,
+        type: 'scatter',
+        mode: 'lines',
+        line: {
+          color: finalLineColor,
+          shape: 'spline',
+          smoothing: 0.8,
+        },
+        hoverlabel: {
+          bgcolor: tooltipBgColor,
+          bordercolor: tooltipBorderColor,
+          font: {
+            family: 'Inter, sans-serif',
+            size: 12,
+            color: textColor,
+          },
+        },
+        customdata: customData,
+        hovertemplate: hoverTemplate,
+      },
+    ]
   }
 
-  const [figure, setFigure] = useState<PlotlyFigure>(
-    {
-      data: getPlotData(xData(), yData()),
-      layout: {
-        plot_bgcolor: 'transparent',
-        paper_bgcolor: 'transparent',
-        transition: {
-          easing: 'cubic-in-out',
-          duration: animationDuration,
-        }
+  const [figure, setFigure] = useState<PlotlyFigure>({
+    data: getPlotData(xData(), yData()),
+    layout: {
+      title: {
+        text: title,
+        font: { color: textColor },
       },
-      frames: [],
-      config: {}
-    }
-  );
+      font: {
+        color: textColor,
+        size: 16,
+      },
+      autosize: true,
+      paper_bgcolor: bgColor,
+      plot_bgcolor: bgColor,
+      xaxis: {
+        title: {
+          text: xAxisLabel,
+          standoff: 5,
+        },
+        automargin: true,
+        showgrid: false,
+        showline: false,
+        gridwidth: 0.4,
+        gridcolor: gridColor,
+      },
+      yaxis: {
+        title: {
+          text: yAxisLabel,
+          standoff: 5,
+        },
+        automargin: true,
+        showgrid: true,
+        showline: false,
+        gridwidth: 0.4,
+        gridcolor: gridColor,
+      },
+      margin: { l: 60, r: 30, t: 35, b: 60 },
+      transition: {
+        easing: 'cubic-in-out',
+        duration: animationDuration,
+      },
+    },
+    frames: [],
+    config: {
+      responsive: true,
+      displaylogo: false,
+      toImageButtonOptions: {
+        format: 'png',
+        filename: 'chart_export',
+      },
+    },
+  })
 
-  const updateDisabledRef = React.useRef(false);
+  const updateDisabledRef = React.useRef(false)
 
   useEffect(() => {
-    const xVals = xData();
-    const yVals = yData();
-  
-    console.log('updateDisabled:', updateDisabledRef.current);
+    const xVals = xData()
+    const yVals = yData()
+
     if (xVals.length > 0 && !updateDisabledRef.current) {
       const newLayout: Partial<Plotly.Layout> = {
         ...figure.layout,
@@ -64,32 +148,30 @@ const TimeSeriesChart: React.FC<TimeSeriesChartProps> = ({
           ...figure.layout.xaxis,
           range: [xVals[0], xVals[xVals.length - 1]], // update range to fit data
         },
-      };
-  
+      }
+
       setFigure((prev) => ({
         ...prev,
         data: getPlotData(xVals, yVals),
         layout: newLayout,
-      }));
+      }))
     }
-  }, [xData, yData]);
-  
+  }, [xData, yData])
 
   return (
     <Plot
       data={figure.data}
       layout={figure.layout}
-      frames={figure.frames}
-      config={figure.config}      
+      frames={figure.frames || []}
+      config={figure.config}
       useResizeHandler={true}
-      style={{ width: '100%', height: '100%' }}
+      style={{ width: '100%', height: height }}
       onInitialized={(fig) => setFigure(fig)}
-      // onUpdate={(fig) => setFigure(fig)}
-      onClick={() => updateDisabledRef.current = true}
-      onDoubleClick={() => updateDisabledRef.current = false}
+      onClick={() => (updateDisabledRef.current = true)}
+      onDoubleClick={() => (updateDisabledRef.current = false)}
       onError={(err) => console.error(err)}
     />
-  );
-};
+  )
+}
 
-export default TimeSeriesChart;
+export default TimeSeriesChart
